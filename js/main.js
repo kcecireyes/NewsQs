df24398d21a1c3a83fdf7ff7a0d3a87061711df5
kcecireyes <kcecireyes@gmail.com>
2014-03-30 14:28:03 -0400

    kinda buggy but with some functionality, also, the heck is going on with the timeouts of the text?

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){
+var votesHash = {};
+var totalVotesHash = {};
+
+$(document).ready(function(){    
     //implement autogrow of the textbox
     $('#searchbox').autosize();
 
@@ -7,13 +10,70 @@ $(document).ready(function(){
         $('#searchbox').select();
     });
     //auto type, fade out, select cursor 
-    autotype("Hallo.", 1500, 80);
+    autotype("Hey.", 1500, 80);
     // autotype("It's election day! Ask me questions.", 1500, 80);   
     handleAnswer(answer, "none");
-    // $("#more_results").click(function(){
-    //     $(this).append("<img src=Logo.svg>");
-    // });
+
 });
+    // swing states
+   function swing_states_graphic() {   
+    $.getJSON("http://ec2-184-73-124-192.compute-1.amazonaws.com/jsonp/results?callback=?", function(data) {
+        var data_Romney = [data.state.CO.race_code.PR.race_candidate_id.PR0800002.votes_total,
+                           data.state.FL.race_code.PR.race_candidate_id.PR1200002.votes_total,
+                           data.state.OH.race_code.PR.race_candidate_id.PR3900002.votes_total,
+                           data.state.VA.race_code.PR.race_candidate_id.PR5100002.votes_total];        
+        var data_Obama = [data.state.CO.race_code.PR.race_candidate_id.PR0800001.votes_total,
+                           data.state.FL.race_code.PR.race_candidate_id.PR1200001.votes_total,
+                           data.state.OH.race_code.PR.race_candidate_id.PR3900001.votes_total,
+                           data.state.VA.race_code.PR.race_candidate_id.PR5100001.votes_total];         
+        $('#results').highcharts({      
+            chart: { type: 'bar' },
+            title: { text: 'Swing States' },            
+            xAxis: { categories: ['Colorado', 'Florida', 'Ohio', 'Virginia'] },
+            yAxis: { min: 0, title: { text: '' } },
+
+            legend: { backgroundColor: '#FFFFFF', reversed: true },
+            plotOptions: {
+                series: { stacking: 'normal'} },
+                series: [{
+                name: 'Romney',
+                data: data_Romney,
+                color: '#FF1919'
+                     
+            }, {
+                name: 'Obama',
+                data: data_Obama,
+                color: '#3333CC'
+            }]
+        });
+        });
+    }
+
+// migratory information 
+    function migratory_info(){
+        $('#results').highcharts({
+            chart: {type: 'bar' },
+            title: {text: 'Key Demographic Groups' },
+            xAxis: {categories: ['Independents', 'Hispanics', 'Youth', 'Women'] },
+            yAxis: {min: 0 ,title: {text: ''} },
+            legend: { backgroundColor: '#FFFFFF', reversed: true },
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+                series: [{
+                name: 'Romney',
+                data: [5, 10, 20, 4],
+                color: '#FF1919'
+                     
+            }, {
+                name: 'Obama',
+                data: [55, 70, 60, 3],
+                color: '#3333CC'
+            }]
+        });
+    }
 
 function handleAnswer(answer_func, answer_type) { //capturing the enter
     $("#searchbox").keypress(function(e) {
@@ -48,17 +108,15 @@ function fadeAndFocus(txt, fadeTime, delay, addNum){
     }, (delay*(txt.length + addNum)) + fadeTime);
 }
 
-function removeAnswer(){
-    console.log("I'm in removeAnswer");
+function removeAnswer(){    
     $("#results").empty();
 }
 
-function entertext(answer) {
+function entertext(answer) {    
     $("#results").append("<h1>" + answer + "</h1>");
 }
 
-function answer(question, type) {
-    console.log('went into answer')
+function answer(question, type) {    
     // FUNSIES DEMO
     if (question == "who's the prettiest?") { entertext("YOU ARE");};
     if (question == "who's the prettiest now?") { entertext("STILL YOU"); };
@@ -70,66 +128,63 @@ function answer(question, type) {
         if (question.match(/^who's\swinning\sin\s\my\s(...)$/)) {        
             getUserState('presidential');
         }
-        if (question.match(/^who's\srunning\sfor\s(\w*)(\?)*$/)) {
-            console.log("matched");
-            var position = question.replace("who's", "").replace("running for", "").replace("?","").replace(/\s+/g, "");
-            console.log(position + " this is the position");
-            if (position == "senate") {
-                console.log("lol senate");
+        if (question.match(/^who's\srunning\sfor\s(\w*)(\?)*$/)) {            
+            var position = question.replace("who's", "").replace("running for", "").replace("?","").replace(/\s+/g, "");            
+            if (position == "senate") {                
                 getUserState('senate');
             }
             else if ((position == "congress") || (position == "representative")) {
-                console.log("lol congress");
                 getUserState('congress');
             }
         }
+        if (question.match(/^(.*\s*)*who\sis\s(\w*)(\?)*$/)) {
+            console.log("match");
+            var name = question.replace(/(.*\s*)*(?=who\sis\s)/, "").replace("who is", "").replace("?", "");
+            console.log(name);
+        }      
     }
 }
 
 function getUserState(question_type) {
-    setTimeout(function(){autotype('What state do you live in?', 1500, 80)}, 2000);
+    console.log("getUserState");
+    setTimeout(function(){ autotype('What state do you live in?', 1500, 80)}, 2000);
     handleAnswer(answerState, question_type);
 }
 
-function answerState(answer, type) {    
+function answerState(answer, type) {
+    console.log("hello");    
     if (answer.match(/^\w\w$/)){
         if (type == "presidential") {
             getCandidateResultsByState(answer);
         }
         else if (type == "senate"){
-            console.log("getCandidateResults by senate");
+            autotype("Sorry, I'm under    construction :(", 1500, 80);
         }
         else if (type == "congress"){
-            console.log("getCandidateResults by congress");
+            autotype("Sorry, I'm under    construction :(", 1500, 80);
         }
     }
 }
-function getCandidateResultsByState(state){
-    $.getJSON("http://ec2-184-73-124-192.compute-1.amazonaws.com/jsonp/results?callback=?", function(data) {
-        console.log(data);
 
+function getCandidateResultsByState(state){
+    $.getJSON("http://ec2-184-73-124-192.compute-1.amazonaws.com/jsonp/results?callback=?", function(data) {        
         var totalVotes = 0;
         for(k in data.state[state].race_code.PR.race_candidate_id){
             totalVotes += data.state[state].race_code.PR.race_candidate_id[k]['votes_total'];
         }
-
-        var votesHash = {};
-        console.log(totalVotes)
-        for(k in data.state[state].race_code.PR.race_candidate_id){
+                
+        for(k in data.state[state].race_code.PR.race_candidate_id){         
             votesHash[data.state[state].race_code.PR.race_candidate_id[k]['name']] = data.state[state].race_code.PR.race_candidate_id[k]['votes_total'];
-            entertext(data.state[state].race_code.PR.race_candidate_id[k]['name'] + ' :: ' 
+            entertext(data.state[state].race_code.PR.race_candidate_id[k]['name'] + ' -- ' 
                 + ((data.state[state].race_code.PR.race_candidate_id[k]['votes_total']/totalVotes)*100).toFixed(2) + "%");
         }
-        console.log(votesHash);
+                
     });
 }
 
 function getCandidateResults(candidate_name) {
     candidate_name = candidate_name.replace(/ /g,'');
-
     $.getJSON("http://ec2-184-73-124-192.compute-1.amazonaws.com/jsonp/results?callback=?", function(data) {
-        totalVotesHash = {}
-        // console.log(data);
         for(key in data.state){
             for(k in data.state[key].race_code.PR.race_candidate_id){
                 if(!totalVotesHash[data.state[key].race_code.PR.race_candidate_id[k]['name']]){
@@ -141,29 +196,43 @@ function getCandidateResults(candidate_name) {
         totalVoteNumber = 0;
         var maxVotes = 0;
         var maxKey;
-        for(key in totalVotesHash){
+        for(key in totalVotesHash){            // 
             totalVoteNumber += totalVotesHash[key];
             if(totalVotesHash[key] > maxVotes){
                 maxKey = key;
                 maxVotes = totalVotesHash[key];
             }
-        }
-        console.log(totalVotesHash);
-        console.log(candidate_name);
+        }                
 
         if(totalVotesHash[candidate_name] == maxVotes){
-            entertext(candidate_name + " is winning with " + ((totalVotesHash[candidate_name]/totalVoteNumber)*100).toFixed(2) + '% of the vote')
+            entertext("YES!")
+            entertext(candidate_name + " is winning with " + ((totalVotesHash[candidate_name]/totalVoteNumber)*100).toFixed(2) + '% of the vote');
         }
         else if(totalVotesHash[candidate_name] < maxVotes){
             entertext(
                 candidate_name + ' is losing with ' + ((totalVotesHash[candidate_name]/totalVoteNumber)*100).toFixed(2) + '% of the vote.' +
                 maxKey + ' has ' + ((maxVotes/totalVoteNumber)*100).toFixed(2) + '% of the vote '
-                )
+                );
         }
         else{
             entertext(
                 'Something was misspelled. Try again.')
         }
-
     });                 
+    showMoreButton("candidates");
+}
+
+function showMoreButton (type_of_show) {    
+    $("body").append("<input id='more_results' type='button' value='Gimmme gimme more!''>");
+    $("#more_results").click(function() {
+        if (type_of_show == "candidates"){
+            swing_states_graphic();
+            $("#results").after($("#searchbox"));
+            hideButton();
+        }
+    });
+}
+
+function hideButton() {
+    $("#more_results").hide();
 }
