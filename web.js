3ea710bba48b53b7178aa4931e513bedfa57c465
kcecireyes <kcecireyes@gmail.com>
2014-04-15 13:37:55 -0400

    app up and running

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
-// web.js
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-        res.send('Hello World!');
+app.use(express.static(__dirname));
+
+app.get("/", function(request, response){
+        response.send("hello!");
     });
 
 var port = Number(process.env.PORT || 5000);
